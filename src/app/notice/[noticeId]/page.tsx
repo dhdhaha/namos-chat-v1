750b8ddd13e621def195893da1d9b1c7ebca9f2a
dhdhaha <psc6664@gmail.com>
2025-08-08 09:32:05 +0900

    공지사항 상세 페이지 타입 수정

--- src/app/notice/[noticeId]/page.tsx
+++ src/app/notice/[noticeId]/page.tsx
@@ -1,14 +1,12 @@
 import NoticeDetailClient from '@/components/NoticeDetailClient';
 
-// Next.js 페이지가 받을 수 있는 모든 props를 포함하는 표준 타입 정의입니다.
-// searchParams를 추가하여 타입을 더 완전하게 만듭니다.
-type Props = {
+// Next.js 표준 PageProps 타입 정의
+interface NoticeDetailPageProps {
   params: { noticeId: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
 
-// 이 페이지는 서버 컴포넌트로서, URL 파라미터를 받아
-// 클라이언트 컴포넌트를 렌더링하고 파라미터를 전달합니다.
-export default function NoticeDetailPage({ params }: Props) {
+// 서버 컴포넌트
+export default function NoticeDetailPage({ params }: NoticeDetailPageProps) {
   return <NoticeDetailClient noticeId={params.noticeId} />;
 }
