0d4924fc65a60a2757b4d1bb1e9ffe5046b933bb
dhdhaha <psc6664@gmail.com>
2025-08-08 09:02:49 +0900

    fix: 안정적인 패키지 버전으로 업데이트하여 빌드 에러 해결

--- src/app/notice/[noticeId]/page.tsx
+++ src/app/notice/[noticeId]/page.tsx
@@ -1,14 +1,14 @@
 import NoticeDetailClient from '@/components/NoticeDetailClient';
 
-// page.tsx가 받는 props의 타입을 정의합니다.
-interface NoticeDetailPageProps {
-  params: {
-    noticeId: string;
-  };
-}
+// Next.js 페이지가 받을 수 있는 모든 props를 포함하는 표준 타입 정의입니다.
+// searchParams를 추가하여 타입을 더 완전하게 만듭니다.
+type Props = {
+  params: { noticeId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 // 이 페이지는 서버 컴포넌트로서, URL 파라미터를 받아
 // 클라이언트 컴포넌트를 렌더링하고 파라미터를 전달합니다.
-export default function NoticeDetailPage({ params }: NoticeDetailPageProps) {
+export default function NoticeDetailPage({ params }: Props) {
   return <NoticeDetailClient noticeId={params.noticeId} />;
 }
