7e35a6f2efa99057e0e5fd744cd6cd96175e4ce1
dhdhaha <psc6664@gmail.com>
2025-08-08 09:37:06 +0900

    공지사항 상세 페이지 타입 수정

--- src/app/notice/[noticeId]/page.tsx
+++ src/app/notice/[noticeId]/page.tsx
@@ -1,12 +1,11 @@
 import NoticeDetailClient from '@/components/NoticeDetailClient';
 
-// Next.js 표준 PageProps 타입 정의
-interface NoticeDetailPageProps {
+// Netlify 호환 버전
+export default function NoticeDetailPage({
+  params,
+}: {
   params: { noticeId: string };
   searchParams?: { [key: string]: string | string[] | undefined };
-}
-
-// 서버 컴포넌트
-export default function NoticeDetailPage({ params }: NoticeDetailPageProps) {
+}) {
   return <NoticeDetailClient noticeId={params.noticeId} />;
 }
