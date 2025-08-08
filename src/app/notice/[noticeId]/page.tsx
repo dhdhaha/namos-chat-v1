a88a646ed0457132b93f4b51b1283bd5c9de8352
dhdhaha <psc6664@gmail.com>
2025-08-08 09:47:22 +0900

    ESLint any 규칙 비활성화 및 타입 수정

--- src/app/notice/[noticeId]/page.tsx
+++ src/app/notice/[noticeId]/page.tsx
@@ -2,11 +2,6 @@ import NoticeDetailClient from '@/components/NoticeDetailClient';
 
 // Netlify 타입 충돌 회피 버전
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export default function NoticeDetailPage({
-  params,
-}: {
-  params: any;
-  searchParams?: Record<string, string | string[] | undefined>;
-})  {
+export default function NoticeDetailPage({ params }: any) {
   return <NoticeDetailClient noticeId={params.noticeId} />;
 }
